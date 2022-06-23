55bfb269c89ce92af7808fd00d918d907666fa86
Phillip <samani.phillip@gmail.com>
2022-06-24 07:28:30 +1100

    Docker compose configured to check for db status

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -9,7 +9,6 @@ from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
-    
     """Django command to wait for database."""
 
     def handle(self, *args, **options):
@@ -26,4 +25,3 @@ class Command(BaseCommand):
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
-
